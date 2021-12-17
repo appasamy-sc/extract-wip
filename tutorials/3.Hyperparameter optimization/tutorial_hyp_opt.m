2d231a597e669d1f856f0db8f311bc15be2a560b
manisha-sinha-appasamy <manisha.sinha@appasamy.com>
2021-12-17 13:09:48 +0530

    Update tutorial_hyp_opt.m
    Added code for reading large matrices by passing the filename as a string.
    Added recommendations for num_partiions_x and num_partitions_y.
    Added change in config in case a non-supported GPU is detected by MATLAB.

--- tutorials/3.Hyperparameter optimization/tutorial_hyp_opt.m
+++ tutorials/3.Hyperparameter optimization/tutorial_hyp_opt.m
@@ -1,16 +1,23 @@
-M = single(hdf5read('jones.hdf5','/Data/Images'));
+% OPTION 1: Uncomment the following if you want to load the entire 3D movie matrix (~11 GB). Not recommeded for systems with limited memory. 
+% M = single(hdf5read('jones.hdf5','/Data/Images'));
 
+% OPTION 2: Instead, pass the movie name as a string. Default option, comment the following if using OPTION 1.
+M = 'jones.hdf5:/Data/Images';
 
-% Decide on the partition number, and play with internal threshold parameters
-
+% Define the configuration to be used
 config=[];
 config = get_defaults(config);
 config.avg_cell_radius=7;
-config.num_partitions_x=1;
-config.num_partitions_y=1;
+
+% If you do not have a native NVIDIA GPU, MATLAB may throw an error. Uncomment the following if that is the case.
+% config.use_gpu=0;
+
+% Decide the number of partitions based on available RAM and GPU memory. Typically, for a system with <=24 GB RAM, upto 10 x and y partitions would work. 
+config.num_partitions_x=10;
+config.num_partitions_y=10;
 
 
-
+% Play with internal threshold parameters
 config.thresholds.T_min_snr=4;
 config.thresholds.spatial_corrupt_thresh=1.5;
 config.cellfind_min_snr=0;
